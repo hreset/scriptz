5cc8330290f105ee0050899343138f13ce8c9e06
hreset <henson.reset@gmail.com>
2015-04-20 12:54:34 -0400

    Added max attempts parameter
    Added max attempts parameter: -att 5
    And also cleaned up the output a bit

--- bounty_test.ps1
+++ bounty_test.ps1
@@ -1,42 +1,58 @@
-﻿ # Use: bounty_test \\someNetworkPath [-log logname.log] [-sec secondsBetween]
+﻿ # bounty_test \\someNetworkPath [-log log.log] [-sec seconds] [-att attempts]
  # henson.reset@gmail.com
 
  # Attaches specified network drive to first available drive letter
- # and tests connection every specified number of seconds.
+ # and tests connection every specified number of seconds. If attempts 
+ # is not specified, script will run until user terminates.
  
  [CmdletBinding()]
  param([Parameter(Mandatory=$true)][string]$netPath,
        [string]$logName = "bountlog.txt",
-       [string]$seconds = 300)
-
- # Number of seconds to wait between attempts
- $seconds = 5
+       [int]$seconds  = 300,
+       [int]$attempts = -1)
 
  # Drive letter of temporary network mapping
  $driveLetter = "NOTASSIGNED"
  
+ # Current attempt number (0-based)
+ $numAttempts = 0
+
+ # Successful attempts
+ $successAttempts = 0
+
  Write-Host "Beginning connection test to $netPath"
  Write-Host "Press Ctrl + C to exit."
- while ($true) { 
+ try {
+     while (($attemps -eq -1) -or ($numAttempts -lt $attempts)) { 
 
-  Start-Sleep -s $seconds
-  if ($driveLetter -ne "NOTASSIGNED") {
-    net use $driveLetter /delete /yes 
-  }
-  
-  #Try Connecting
-  $date = Get-Date
-  $connect  = (net use * $netPath)
-  $lastExit = $LASTEXITCODE
-  $matches  = ($connect | Select-String "Drive\s(.+?)\sis").Matches
+      Start-Sleep -s $seconds
 
-  if (($lastExit -eq 0) -and ($matches.Groups.Count -gt 1)) { 
-    # If successfull
-    $driveLetter = $matches.Groups[1].Value
-    echo "$date - Connected Successfully. ($driveLetter)" >> $logName
-  } else { 
-    # If not successful
-    echo "$date - Could not connect." >> $logName
-  } 
+      # Try Connecting
+      $date = Get-Date
+      $connect  = (net use * $netPath)
+      $lastExit = $LASTEXITCODE
+      $matches  = ($connect | Select-String "Drive\s(.+?)\sis").Matches
 
-} 
+      if (($lastExit -eq 0) -and ($matches.Groups.Count -gt 1)) { 
+        # If successfull
+        $driveLetter = $matches.Groups[1].Value
+        echo "$date - Connected Successfully. ($driveLetter)" >> $logName
+        Write-Debug "Connection attempt $numAttempts was successful."
+        $successAttempts++
+      } else { 
+        # If not successful
+        echo "$date - Could not connect." >> $logName
+        Write-Debug "Connection attempt $numAttempts has failed."
+      } 
+
+      if ($driveLetter -ne "NOTASSIGNED") {
+        # Yes flag is undocumented
+        net use $driveLetter /delete /yes 1> $null
+      }
+      $numAttempts++
+    } 
+ } finally {
+    Write-Host ("Done. {0} of {1} attemps were successful." -f 
+        $successAttempts, $numAttempts)
+        
+ }
